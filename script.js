90f25beb0f6d56d0a08c86ef4e1a0a05b1140684
Rajkirantagde <121598976+rajutagde@users.noreply.github.com>
2023-03-20 17:17:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 //your JS code here. If required.
+const form = document.querySelector("form");
+const nameInput = document.querySelector("#name");
+const ageInput = document.querySelector("#age");
+const submitBtn = document.querySelector("#btn");
+
+submitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (!nameInput.value || !ageInput.value) {
+    alert("Inputs cannot be empty.");
+    return;
+  }
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const age = parseInt(ageInput.value);
+      if (age >= 18) {
+        resolve({ age, name: nameInput.value });
+      } else {
+        reject({ age, name: nameInput.value });
+      }
+    }, 4000);
+  });
+
+  promise
+    .then((result) => {
+      const { name } = result;
+      alert(`Welcome, ${name}. You can vote.`);
+      return result.age;
+    })
+    .then((age) => {
+      return { canVote: true, age };
+    })
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      const { name } = error;
+      alert(`Oh sorry ${name}. You aren't old enough.`);
+    });
+});
